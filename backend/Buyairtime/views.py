a213dc69e8267d4bd8d16ccf6692b058aa27b837
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-05-21 21:10:35 +0100

    update commit

--- backend/Buyairtime/views.py
+++ backend/Buyairtime/views.py
@@ -12,18 +12,19 @@ import json
 def buyairtime():
     url = "https://www.arrifqubundle.com/api/topup/"
     payload = {"network": 1, "amount": 50, "mobile_number": 8063641230,
-               "Ported_number":True, "airtime_type": "VTU"}
+               "Ported_number": True, "airtime_type": "VTU"}
 # k = json.dumps(payload, indent=4)
 #     print("payload", k)
     headers = {
         'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
         'Content-Type': 'application/json'
     }
-    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+    response = requests.request(
+        "POST", url, headers=headers, data=json.dumps(payload))
     print("airtime we", response.text)
     return
 
-    
+
 class BuyAirtime(APIView):
     def get(self, request):
         apikey = "JOBY94XGC4E8345NB27845W03TU0M3IFG6D0IFY923U118CDENU854SS361OG8A9"
@@ -36,14 +37,14 @@ class BuyAirtime(APIView):
             "Z0U5H6E1E407E4GOBFL268B463S6260KNU2CADW1U762774KP53UZW0J1GU4D8VE")
       #   MobileNetwork = urllib.parse.quote("01")
         Amount = urllib.parse.quote("50")
-        MobileNumber = urllib.parse.quote("08063641230")
+        # MobileNumber = urllib.parse.quote("08063641230")
         CallBackURL = urllib.parse.quote("www.arrifqubundle.com")
 # https://www.arrifqubundle.com
         # UserID="CK10136160"
         # APIKey= "Z0U5H6E1E407E4GOBFL268B463S6260KNU2CADW1U762774KP53UZW0J1GU4D8VE"
         MobileNetwork = "01"
         # Amount="50"
-        # MobileNumber="08063641230"
+        MobileNumber=8063641230
         # CallBackURL="/"
 
         # base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp?UserID={0}&APIKey={1}&MobileNetwork={2}&Amount={3}&MobileNumber={4}&CallBackURL={5}'.format(UserID, APIKey, MobileNetwork, Amount, MobileNumber, CallBackURL)
@@ -58,17 +59,16 @@ class BuyAirtime(APIView):
         # response = requests.request("GET","https://www.nellobytesystems.com/APIAirtimeV1.asp?", params=params)
         return Response({"ip": ip.content, "airtime": airtime.content})
 
-    # def post(self, request):
-    #    url = "https://www.arrifqubundle.com/api/user/"
-    #    payload =  {"network": 1,
-    #       "amount" :50,
-    #       "mobile_number": "08063641230",
-    #       "Ported_number":True,
-    #       "airtime_type":"VTU"}
-    #    headers = {
-    #    'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
-    #    'Content-Type': 'application/json'
-    #    }
+    def post(self, request):
+        url = "https://www.arrifqubundle.com/api/topup/"
+        payload = {"network": 1, "amount": 50, "mobile_number": 8063641230,
+                   "Ported_number": True, "airtime_type": "VTU"}
 
-    #    response = requests.request("POST", url, headers=headers, data=payload)
-    #    return Response(response.text)
+        headers = {
+            'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
+            'Content-Type': 'application/json'
+        }
+        response = requests.request(
+            "POST", url, headers=headers, data=json.dumps(payload))
+        print("airtime we", response.text)
+        return Response(response.text)
