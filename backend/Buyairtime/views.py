a75636be2da77a552cb49fc0bcfbe7e08ad95290
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-05-23 08:30:52 +0100

    update commit

--- backend/Buyairtime/views.py
+++ backend/Buyairtime/views.py
@@ -1,72 +1,70 @@
 from django.shortcuts import render
-import requests
+# import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
-from django.contrib.auth.models import User
-import urllib.parse
+
 import json
 # Create your views here.
 
 
-def buyairtime():
-    url = "https://www.arrifqubundle.com/api/topup/"
-    payload = {"network": 1, "amount": 50, "mobile_number": 8063641230,
-               "Ported_number": True, "airtime_type": "VTU"}
-# k = json.dumps(payload, indent=4)
-#     print("payload", k)
-    headers = {
-        'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
-        'Content-Type': 'application/json'
-    }
-    response = requests.request(
-        "POST", url, headers=headers, data=json.dumps(payload))
-    print("airtime we", response.text)
-    return
+# def buyairtime():
+#     url = "https://www.arrifqubundle.com/api/topup/"
+#     payload = {"network": 1, "amount": 50, "mobile_number": 8063641230,
+#                "Ported_number": True, "airtime_type": "VTU"}
+# # k = json.dumps(payload, indent=4)
+# #     print("payload", k)
+#     headers = {
+#         'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
+#         'Content-Type': 'application/json'
+#     }
+#     response = requests.request(
+#         "POST", url, headers=headers, data=json.dumps(payload))
+#     print("airtime we", response.text)
+#     return
 
 
-def CK():
+# def CK():
 
-    payloadCK = {
+#     payloadCK = {
 
-        "UserID": "CK10136160",
-        "APIKey": "Z0U5H6E1E407E4GOBFL268B463S6260KNU2CADW1U762774KP53UZW0J1GU4D8VE",
-        "Amount": 50,
-        "MobileNetwork": "01",
-        "MobileNumber": 8063641230,
-        "CallBackURL": "/"
-    }
-    base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp'
-    airtime = requests.get(base_url, params=json.dumps(payloadCK))
-    print("airitme", airtime.text)
-    return  # Response({"ip": ip.content, "airtime": airtime.content})
-    # base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp?UserID={0}&APIKey={1}&MobileNetwork={2}&Amount={3}&MobileNumber={4}&CallBackURL={5}'.format(UserID, APIKey, MobileNetwork, Amount, MobileNumber, CallBackURL)
-   #   base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp?UserID={UserID}&APIKey={APIKey}&MobileNetwork={MobileNetwork}&Amount={Amount}&MobileNumber={MobileNumber}&CallBackURL={CallBackURL}'
-   #   base_url = f'https://www.nellobytesystems.com/APIWalletBalanceV1.asp?UserID={UserID}&APIKey={APIKey}'
-    # ip_url = f'https://www.nellobytesystems.com/APIServerIPV1.asp?UserID={UserID}&APIKey={APIKey}'
-    # ip = requests.get(ip_url)
+#         "UserID": "CK10136160",
+#         "APIKey": "Z0U5H6E1E407E4GOBFL268B463S6260KNU2CADW1U762774KP53UZW0J1GU4D8VE",
+#         "Amount": 50,
+#         "MobileNetwork": "01",
+#         "MobileNumber": 8063641230,
+#         "CallBackURL": "/"
+#     }
+#     base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp'
+#     airtime = requests.get(base_url, params=json.dumps(payloadCK))
+#     print("airitme", airtime.text)
+#     return  # Response({"ip": ip.content, "airtime": airtime.content})
+#     # base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp?UserID={0}&APIKey={1}&MobileNetwork={2}&Amount={3}&MobileNumber={4}&CallBackURL={5}'.format(UserID, APIKey, MobileNetwork, Amount, MobileNumber, CallBackURL)
+#    #   base_url = f'https://www.nellobytesystems.com/APIAirtimeV1.asp?UserID={UserID}&APIKey={APIKey}&MobileNetwork={MobileNetwork}&Amount={Amount}&MobileNumber={MobileNumber}&CallBackURL={CallBackURL}'
+#    #   base_url = f'https://www.nellobytesystems.com/APIWalletBalanceV1.asp?UserID={UserID}&APIKey={APIKey}'
+#     # ip_url = f'https://www.nellobytesystems.com/APIServerIPV1.asp?UserID={UserID}&APIKey={APIKey}'
+#     # ip = requests.get(ip_url)
 
-    # response = requests.request("GET","https://www.nellobytesystems.com/APIAirtimeV1.asp?", params=params)
+#     # response = requests.request("GET","https://www.nellobytesystems.com/APIAirtimeV1.asp?", params=params)
 
 
 class BuyAirtime(APIView):
 
     def post(self, request):
+       
+        print("data", request.data)
+        # url = "https://www.arrifqubundle.com/api/topup/"
+        # payload = {"network": 1, "amount": 50, "mobile_number": 8063641230,
+        #            "Ported_number": True, "airtime_type": "VTU"}
 
-        print("posted request", request)
-        url = "https://www.arrifqubundle.com/api/topup/"
-        payload = {"network": 1, "amount": 50, "mobile_number": 8063641230,
-                   "Ported_number": True, "airtime_type": "VTU"}
+        # headers = {
+        #     'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
+        #     'Content-Type': 'application/json'
+        # }
+        # response = requests.request(
+        #     "POST", url, headers=headers, data=json.dumps(payload))
+        # if response.status_code == 200:
+        #     airtime = json.loads(response.content)
+        # else:
+        #     airtime = json.loads(response.content)
 
-        headers = {
-            'Authorization': 'Token 4f81017a394ceaaf050456dc31425d7395121712',
-            'Content-Type': 'application/json'
-        }
-        response = requests.request(
-            "POST", url, headers=headers, data=json.dumps(payload))
-        if response.status_code == 200:
-            airtime = json.loads(response.content)
-        else:
-            airtime = json.loads(response.content)
-
-        return Response(airtime)
+        return Response("airtime")
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,9 +74,35 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+
+CORS_ALLOW_ALL_ORIGINS: True
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    "Access-Control-Allow-Origin", "*",
+    "Access-Control-Allow-Headers"
+]
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
