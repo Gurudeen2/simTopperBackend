a62741ede3bf4d3c3a04c64f966f8512a9e2c8d9
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-10-01 00:20:14 +0100

    fund app backend done

--- backend/wallet/views.py
+++ backend/wallet/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, serializers, status
 from .models import Wallet
-import json
+
 
 
 # Create your views here.
